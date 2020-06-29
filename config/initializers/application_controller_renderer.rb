e99d75de64d6c036966ce8ef073e6c792b7e6f9d
kotaro-yamada <kamakurav0808@gmail.com>
2020-06-29 14:14:56 +0900

    completed varsion

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'b959fde58143da5689089a1acac1f5fe4c6ae5e6f50fc409b3b795ee28a5f362aa7a0f89b5301e901a35d1da9ca3f430959305465dbf040b0e25be947c6b5255'
